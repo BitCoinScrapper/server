18b504b9804fd9c9ff18774c0fb91af6dcdd3597
abdulmukmin <abdmukmin@rocketmail.com>
2018-11-29 18:09:48 +0700

    frist skeleton

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const cors = require('cors');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var app = express();
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/BitCoinScraping', { useNewUrlParser: true });
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log(`mongoose connected!`)
+});
+
+app.use(cors())
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
